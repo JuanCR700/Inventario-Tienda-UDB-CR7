20a2bdde0773f101001aff97df8b9a203f744f75
JC <ssjramirez69@gmail.com>
2025-03-08 15:49:25 -0600

    controles pestana productos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,25 @@
         {
             this.tabControlInventario = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.txtIDProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecioProducto = new System.Windows.Forms.TextBox();
+            this.txtStockProducto = new System.Windows.Forms.TextBox();
+            this.cbCategriaProducto = new System.Windows.Forms.ComboBox();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.btnMostrarStock = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
             this.tabControlInventario.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlInventario
@@ -47,25 +57,69 @@
             this.tabControlInventario.Controls.Add(this.tabPage2);
             this.tabControlInventario.Controls.Add(this.tabPage3);
             this.tabControlInventario.Controls.Add(this.tabPage4);
-            this.tabControlInventario.Location = new System.Drawing.Point(38, 99);
+            this.tabControlInventario.Location = new System.Drawing.Point(37, 12);
             this.tabControlInventario.Name = "tabControlInventario";
             this.tabControlInventario.SelectedIndex = 0;
-            this.tabControlInventario.Size = new System.Drawing.Size(689, 323);
+            this.tabControlInventario.Size = new System.Drawing.Size(511, 306);
             this.tabControlInventario.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.label4);
-            this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.cbCategriaProducto);
+            this.tabPage1.Controls.Add(this.txtStockProducto);
+            this.tabPage1.Controls.Add(this.txtPrecioProducto);
+            this.tabPage1.Controls.Add(this.txtNombreProducto);
+            this.tabPage1.Controls.Add(this.txtIDProducto);
+            this.tabPage1.Controls.Add(this.lblStock);
+            this.tabPage1.Controls.Add(this.lblPrecio);
+            this.tabPage1.Controls.Add(this.lblCategoria);
+            this.tabPage1.Controls.Add(this.lblNombre);
+            this.tabPage1.Controls.Add(this.lblID);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(681, 294);
+            this.tabPage1.Size = new System.Drawing.Size(503, 277);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Productos";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(23, 183);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(23, 136);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(66, 16);
+            this.lblCategoria.TabIndex = 2;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(23, 83);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            this.lblNombre.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(23, 41);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(20, 16);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            this.lblID.Click += new System.EventHandler(this.label1_Click);
             // 
             // tabPage2
             // 
@@ -94,58 +148,103 @@
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage4.Size = new System.Drawing.Size(681, 294);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Consultar Ventas";
+            this.tabPage4.Text = "Estadisticas";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblStock
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(23, 235);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(41, 16);
+            this.lblStock.TabIndex = 4;
+            this.lblStock.Text = "Stock";
             // 
-            // label2
+            // txtIDProducto
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(175, 52);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.txtIDProducto.AcceptsReturn = true;
+            this.txtIDProducto.Location = new System.Drawing.Point(103, 35);
+            this.txtIDProducto.Name = "txtIDProducto";
+            this.txtIDProducto.Size = new System.Drawing.Size(377, 22);
+            this.txtIDProducto.TabIndex = 5;
             // 
-            // label3
+            // txtNombreProducto
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(299, 56);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.txtNombreProducto.Location = new System.Drawing.Point(103, 77);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(377, 22);
+            this.txtNombreProducto.TabIndex = 6;
             // 
-            // label4
+            // txtPrecioProducto
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(429, 61);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.txtPrecioProducto.Location = new System.Drawing.Point(103, 177);
+            this.txtPrecioProducto.Name = "txtPrecioProducto";
+            this.txtPrecioProducto.Size = new System.Drawing.Size(377, 22);
+            this.txtPrecioProducto.TabIndex = 8;
+            // 
+            // txtStockProducto
+            // 
+            this.txtStockProducto.Location = new System.Drawing.Point(103, 229);
+            this.txtStockProducto.Name = "txtStockProducto";
+            this.txtStockProducto.Size = new System.Drawing.Size(377, 22);
+            this.txtStockProducto.TabIndex = 9;
+            // 
+            // cbCategriaProducto
+            // 
+            this.cbCategriaProducto.FormattingEnabled = true;
+            this.cbCategriaProducto.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.cbCategriaProducto.Location = new System.Drawing.Point(103, 128);
+            this.cbCategriaProducto.Name = "cbCategriaProducto";
+            this.cbCategriaProducto.Size = new System.Drawing.Size(377, 24);
+            this.cbCategriaProducto.TabIndex = 10;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(611, 75);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(135, 23);
+            this.btnAgregarProducto.TabIndex = 1;
+            this.btnAgregarProducto.Text = "Agregar Producto";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrarStock
+            // 
+            this.btnMostrarStock.Location = new System.Drawing.Point(611, 138);
+            this.btnMostrarStock.Name = "btnMostrarStock";
+            this.btnMostrarStock.Size = new System.Drawing.Size(135, 23);
+            this.btnMostrarStock.TabIndex = 2;
+            this.btnMostrarStock.Text = "Mostrar Stock";
+            this.btnMostrarStock.UseVisualStyleBackColor = true;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(37, 324);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(511, 150);
+            this.dgvProductos.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnMostrarStock);
+            this.Controls.Add(this.btnAgregarProducto);
             this.Controls.Add(this.tabControlInventario);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControlInventario.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -157,10 +256,19 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.TextBox txtStockProducto;
+        private System.Windows.Forms.TextBox txtPrecioProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtIDProducto;
+        private System.Windows.Forms.ComboBox cbCategriaProducto;
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.Button btnMostrarStock;
+        private System.Windows.Forms.DataGridView dgvProductos;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,8 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Inventario_Machote
 {
+
+
     public partial class Form1: Form
     {
         public Form1()
